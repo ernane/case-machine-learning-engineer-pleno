9f0fe7b52bd1c93c13d4122d45abc30c4ce794e7
Ernane Sena <ernane.sena@gmail.com>
2024-10-11 15:56:25 -0300

    feat: Adicionar roteador para histórico de modelo
    Adicionar roteador para histórico de modelo no arquivo main.py do driver REST.

--- src/drivers/rest/main.py
+++ src/drivers/rest/main.py
@@ -1,9 +1,15 @@
 from fastapi import FastAPI
 
-from src.drivers.rest.routers import health, model_load, model_predict
+from src.drivers.rest.routers import (
+    health,
+    model_history,
+    model_load,
+    model_predict,
+)
 
 app = FastAPI()
 
 app.include_router(health.router)
 app.include_router(model_load.router)
 app.include_router(model_predict.router)
+app.include_router(model_history.router)
